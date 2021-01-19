a0c953d8c08577fdbae5c08d20a25cbd622fb919
Huzaifa Saboowala <huzaifa.saboowala96@gmail.com>
2021-01-19 16:14:34 -0500

    Added OnClickListener for the button that displays a Toast message

--- app/src/main/java/com/huzaifa/androidgitintegration/MainActivity.java
+++ app/src/main/java/com/huzaifa/androidgitintegration/MainActivity.java
@@ -3,6 +3,9 @@ package com.huzaifa.androidgitintegration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clickMeBtn = (Button) findViewById(R.id.clickmebutton);
+        clickMeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Welcome to Huzaifa Saboowala's App",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
